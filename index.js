38c6d87ac5c2d18a02db40042f6b067125cc86f3
dodokjr <ffikri604@gmail.com>
2023-02-17 19:01:29 +0700

    3

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const port = 5000
 const cors = require('cors')
 
 const github = require('./routes/github/api');
-const anime = require('./routes/anime/api')
-const crypto = require('./routes/crypto/api')
+const anime = require('./routes/anime/api');
+const crypto = require('./routes/crypto/api');
+const public = require('./routes/public/api');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -22,58 +23,21 @@ app.use(function (req, res, next)
 app.use('/v3/', crypto);
 app.use('/v2/', anime);
 app.use('/v1/', github);
-app.post('/', function (req, res)
+app.get('/', function (req, res)
 {
      res.json({ message: "Selamat datang di api mfikria " });
-     console.log(res);
-});
 
-app.use(express.static(__dirname + '/public'));
-app.get('/img/1', function (req, res)
+});
+app.get('/media-url', function (req, res)
 {
-     res.sendFile(__dirname + '/public/assets/about.png');
-});
-
-app.get('/img/2', function (req, res)
-{
-     res.sendFile(__dirname + '/public/assets/me.webp');
-});
-
-app.get('/img/3', function (req, res)
-{
-     res.sendFile(__dirname + '/public/assets/giphy1.gif');
-});
-
-app.get('/img/4', function (req, res)
-{
-     res.sendFile(__dirname + '/public/assets/giphy.gif');
-});
-
-app.get('/img/5', function (req, res)
-{
-     res.sendFile(__dirname + '/public/assets/milly-anime.gif');
-});
-
-app.get('/img/6', function (req, res)
-{
-     res.sendFile(__dirname + '/public/assets/shiki-mori-shikimoris-not-just-cute.gif');
-});
-
-app.get('/img/7', function (req, res)
-{
-     res.sendFile(__dirname + '/public/assets/DodokStore.png');
-});
-
-app.get('/img/8', function (req, res)
-{
-     res.sendFile(__dirname + '/public/assets/Background.png');
-});
-
-app.get('/vidio/1', function (req, res)
-{
-     res.sendFile(__dirname + '/public/assets/video/Official髭男dism  I LOVEOfficial Video_1080p.mp4');
-});
-
+     res.json({
+          github: "https://github.com/dodokjr",
+          instagram: "https://www.instagram.com/fkri__17/",
+          youtube: "https://www.youtube.com/channel/UCLP0I71nvbJ2D_Y5y-mwbEw",
+          linkendi: "https://www.linkedin.com/in/muhammad-fikri-ardiyansah-952752194/",
+          discord: "https://discord.gg/DnNTUuGP"
+     })
+})
 
 app.listen(port, () =>
 {
