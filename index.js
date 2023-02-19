d50d14786d6d6610afe61c012ee45fd0980f0f18
dodokjr <ffikri604@gmail.com>
2023-02-19 16:24:20 +0700

    fffff

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(function (req, res, next)
      res.setHeader('Access-Control-Allow-Origin', 'https://mfikria.netlify.app/', '*');
      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+     res.setHeader('Vary', '*');
      res.setHeader('Access-Control-Allow-Credentials', true);
      next()
 });
