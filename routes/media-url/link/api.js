54f95c1e4024d457cbd0d3e5e17cc8e51eb63c9c
Dodokjr <ffikri604@gmail.com>
2023-03-20 23:29:44 +0700

    ffff

--- routes/media-url/link/api.js
+++ routes/media-url/link/api.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+
+const port = 'https:'
+const ytb = `${port}/www.youtube.com/channel/UCLP0I71nvbJ2D_Y5y-mwbEw?sub_confirmation=1`;
+const ig = `${port}//www.instagram.com/fkri__17/`;
+const gb = `${port}//github.com/dodokjr`;
+const ld = `${port}//www.linkedin.com/in/muhammad-fikri-ardiyansah-952752194/`;
+const dc = `${port}//discord.gg/xHQhrDm4dA`;
 router.get("/", function (res, req)
 {
      req.json({
-          github: "https://github.com/dodokjr",
-          instagram: "https://www.instagram.com/fkri__17/",
-          youtube: "https://www.youtube.com/channel/UCLP0I71nvbJ2D_Y5y-mwbEw",
-          linkedin: "https://www.linkedin.com/in/muhammad-fikri-ardiyansah-952752194/",
-          discord: "https://discord.gg/vSKqthYh"
+          github: gb,
+          instagram: ig,
+          youtube: ytb,
+          linkedin: ld,
+          discord: dc
      })
 })
 
