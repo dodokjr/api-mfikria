fb600cfd065a4b8d95e9aed5f53887bf448b877c
Dodokjr <ffikri604@gmail.com>
2024-01-18 21:37:24 +0700

    sss

--- __public/public/js/api.js
+++ __public/public/js/api.js
@@ -1,7 +1,10 @@
-async function getCard()
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = 'da7c0ba1-bb94-44b9-8bd5-53e67ebdfe81';
+(function ()
 {
-    const queryUrl = `/v2/anime`
-    const response = await fetch(queryUrl);
-    const CardResponse = await response.json();
-    return CardResponse;
-}
+    d = document;
+    s = d.createElement('script');
+    s.src = 'https://client.crisp.chat/l.js';
+    s.async = 1;
+    d.getElementsByTagName('head')[0].appendChild(s);
+})();
