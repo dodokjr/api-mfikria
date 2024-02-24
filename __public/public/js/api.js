8e2573c054f864b0df57535c304b34c4e65c9291
Dodokjr <ffikri604@gmail.com>
2024-02-24 20:14:22 +0700

    koplok

--- __public/public/js/api.js
+++ __public/public/js/api.js
@@ -3,22 +3,19 @@ async function testApi()
     const Bhidden = document.getElementById("hidden")
 
     // Api call 
-    let base = "/v2/anime?page=1&limit=8"
+    let base = "/lk21/movies"
     const req = await fetch(base)
     const items = await req.json()
 
     // Api Res 
-    const res = () => items.data.map(item =>
+    const res = () => items.map(item =>
     {
         const html = `
             <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <img class="rounded-t-lg" src="${item.images.webp.image_url}" alt=".." width="350" height="350"/>
+            <img class="rounded-t-lg" src="${item.posterImg}" alt=".." width="350" height="350"/>
 <div class="p-5">
     <a href="#">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${item.title}</h5>
-    </a>
-    <a href="https://mfanimelist.vercel.app/anime/${item.mal_id}" target="_Blank" class="inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        MfAnimeList
     </a>
 </div>
 </div>
@@ -27,8 +24,7 @@ async function testApi()
     })
     res()
 
-    if (res)
-    {
-        Bhidden.style.display = "none"
-    }
+    if (res) return Bhidden.style.display = "none"
+    if (!res) return innerHTML = "<div>Loading</div>"
 }
+
