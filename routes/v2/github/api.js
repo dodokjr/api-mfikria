9f4fc231467dbc9d095abf1c4ccdf47d5ad4c438
Dodokjr <ffikri604@gmail.com>
2024-04-10 12:24:42 +0700

    fix:bug file

--- routes/v2/github/api.js
+++ routes/v2/github/api.js
@@ -13,7 +13,7 @@ router.get('/:user', async function (req, res)
         headers: {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
         },
-        OAuth: "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"
+        OAuth: "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"
     }
     https.get(options, function (apiResponse)
     {
@@ -58,6 +58,55 @@ router.get('/:user/repos/:reposname', async function (req, res)
     const options = {
         hostname: 'api.github.com',
         path: '/repos/' + `${user}/` + reposname,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
+        },
+        OAuth: "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"
+    }
+    https.get(options, function (apiResponse)
+    {
+        apiResponse.pipe(res);
+        res.set('Content-Type', 'application/json')
+    }).on('error', (e) =>
+    {
+        console.log(ee);
+        res.status(500).send('Api salah')
+    })
+})
+
+router.get('/:user/repos/:reposname/events', async function (req, res)
+{
+    const user = req.params.user;
+    const username = req.params.username;
+    const reposname = req.params.reposname;
+    const options = {
+        hostname: 'api.github.com',
+        path: '/repos/' + `${user}/` + reposname + '/events',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
+        },
+        OAuth: "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"
+    }
+    https.get(options, function (apiResponse)
+    {
+        apiResponse.pipe(res);
+        res.set('Content-Type', 'application/json')
+    }).on('error', (e) =>
+    {
+        console.log(ee);
+        res.status(500).send('Api salah')
+    })
+})
+
+
+router.get('/:user/repos/:reposname/languages', async function (req, res)
+{
+    const user = req.params.user;
+    const username = req.params.username;
+    const reposname = req.params.reposname;
+    const options = {
+        hostname: 'api.github.com',
+        path: '/repos/' + `${user}/` + reposname + '/languages',
         headers: {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
         },
